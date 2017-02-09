e43d0c114ab792400c2a9560f21603f14b166fed
Adam Goth <adamgoth@gmail.com>
2017-02-09 07:09:56 -0700

    Adds game over screen and UI

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,20 +6,28 @@ public class LevelManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		print (SceneManager.GetActiveScene ().buildIndex + " start");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) 
+		/*if (Input.GetKeyDown (KeyCode.Space)) 
 		{
 			LoadNextScene ();
 		}
+		*/
 	}
 
-	void LoadNextScene()
+	public void LoadNextScene()
 	{
 		int currentIndex = SceneManager.GetActiveScene ().buildIndex;
 		SceneManager.LoadScene (currentIndex + 1);
+		print ("next scene called");
+	}
+
+	public void LoadPreviousScene()
+	{
+		int currentIndex = SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentIndex - 1);
 	}
 }
